fe15599f333ebd8654cc46c18ee10ee5c9064ba3
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-04-04 21:17:39 -0500

    update

--- Space Shooter/Assets/Scripts/EnemySpawner.cs
+++ Space Shooter/Assets/Scripts/EnemySpawner.cs
@@ -33,27 +33,29 @@ public class EnemySpawner : MonoBehaviour
         //pick a random enemy to instantiate
         int ndx = Random.Range(0, prefabEnemies.Length);
         /*The Random.Range will never select the max value*/
+
         GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]);
+
         //Position the enemy above the screne with a random x position
         float enemyPadding = enemyDefaultPadding;
+
         if (go.GetComponent<BoundsCheck>() != null)
         { enemyPadding = Mathf.Abs(go.GetComponent<BoundsCheck>().radius); }
+
         //Set the initial position
         Vector3 pos = Vector3.zero;
         float xMin = -bndCheck.camWidth + enemyPadding;
         float xMax = bndCheck.camWidth - enemyPadding;
+
         pos.x = Random.Range(xMin, xMax);
         pos.y = bndCheck.camHeight + enemyPadding;
+
         pos.x = Random.Range(xMin, xMax); //range between the xmin and xmax
         pos.y = bndCheck.camHeight + enemyPadding; // height plus padding, off
         go.transform.position = pos;
         //invoke again
         Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
+
     } // end SpawnEnemy()
 
-        // Update is called once per frame
-        void Update()
-    {
-        
-    }
 }
