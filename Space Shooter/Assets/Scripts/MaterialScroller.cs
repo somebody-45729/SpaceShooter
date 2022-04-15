6451009532e2f69fa633c40d8d2b163111da0a3d
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-04-15 00:20:57 -0500

    update

--- Space Shooter/Assets/Scripts/MaterialScroller.cs
+++ Space Shooter/Assets/Scripts/MaterialScroller.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class MaterialScroller : MonoBehaviour
 {
+
+    /***VARIABLES***/
+    public Vector2 scrollSpeed = new Vector2(0, 0f); //x and y speed for scroll
+    private Renderer goRenderer; //game object's renderer component
+    private Material goMat; //the game object's material
+    private Vector2 offset; //the offset of scroll
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-    }
+        goRenderer = GetComponent<Renderer>();
+        goMat = goRenderer.material;
+    }//end Start()
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        offset = (scrollSpeed * Time.deltaTime); //set offset value over time
+        goMat.mainTextureOffset += offset; //set texture offeset
+    }//end Update()
 }
