eb82b331991d71f3b0997ecea86433a96a7c6ed7
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-04-15 00:05:51 -0500

    update

--- Space Shooter/Assets/Scripts/PoolReturn.cs
+++ Space Shooter/Assets/Scripts/PoolReturn.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PoolReturn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private ObjectPool pool;
+    //Start is called once before the update
+    private void Start()
     {
-        
-    }
+        pool = ObjectPool.POOL; //find the game manager
+    }//end Start()
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
-    }
+        //if the pool is not empty
+        if (pool != null)
+        {
+            pool.ReturnObject(this.gameObject); //return to pool
+        }//end if ( pool != null)
+    }//end OnDisable()
 }
