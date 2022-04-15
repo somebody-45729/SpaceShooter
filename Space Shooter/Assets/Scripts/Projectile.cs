d792db6a18bd7a197b8b705bb574912bcb3a1251
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-04-14 23:15:21 -0500

    update

---
+++ Space Shooter/Assets/Scripts/Projectile.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private BoundsCheck bndCheck; //reference to the bounds check
+    private void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }//end Awake()
+     // Update is called once per frame
+    void Update()
+    {
+        //if off screen up , destroy
+        if (bndCheck.offUp)
+        {
+            Destroy(gameObject);
+        }
+    }//end Update()
+}
